e686ebcf7c425f162b7a7a8a1f527b63d2d62d89
Sergio Gutierrez <ser-1998@hotmail.com>
2023-01-08 13:55:27 -0500

    Release 0.0.0 - Finish script

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import pywhatkit
 import pandas as pd
 import os
+import time
 
-def send_message(data_path:str = None):
+def send_message(project_path):
     "Send message using a DataFrame"
-    data = pd.read_excel(data_path)
-    print(data['Nombre'][0])
-    print(type(data['Nombre'][0]))
+    # Define data excel path
+    data_path = os.path.join(project_path, "data.xlsx")
+
+    # Define Images directory path
+    image_path = os.path.join(project_path, "Images")
+
+    # Read DataFrame data
+    data = pd.read_excel(data_path, dtype={'Celular':str})
     
-    for i in (0,len(data)):
+    # Send messages 
+    for i in range(0,len(data)):
         name = data['Nombre'][i]
         number = data['Celular'][i]
         message = data['Mensaje'][i]
-        image = data['Imagen'][i]
+        image = os.path.join(image_path, data['Imagen'][i])
 
+        print(f'Enviar mensaje a {name} con numero {number}') 
+ 
         pywhatkit.sendwhats_image(receiver=number, 
-                                  img_path=r'D:\Personal\Personal Projects\Python-WhatsApp\Images\robot_hi.png', 
+                                  img_path=image, 
                                   tab_close=True,
-                                  caption=f'Hola {Prueba bot {i}',
+                                  caption=message,
                                   wait_time=15)
+        time.sleep(2)
 
 if __name__ == '__main__':
-    send_message(data_path=r'./data.xlsx')
+    #Eg: D:\Personal\Personal Projects\Python-WhatsApp
+    send_message(project_path= r'D:\Personal\Personal Projects\Python-WhatsApp') 
